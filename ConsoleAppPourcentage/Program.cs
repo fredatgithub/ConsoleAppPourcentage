e0718d5508e3a48ded35efe93e1adb65474431f5
fred <domaine75@gmail.com>
2024-11-10 01:19:22 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppPourcentage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleAppPourcentage
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i <= 100; i++)
+      {
+        Console.Write($"\rProgression : {i}%");
+        Thread.Sleep(50);
+      }
+
+      Console.WriteLine("\nOpération terminée !");
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
